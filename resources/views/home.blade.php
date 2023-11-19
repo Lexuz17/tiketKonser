d5a37456b3ae95ddeee6b9632f005281d2efbac5
Lexuz <90095821+Lexuz17@users.noreply.github.com>
2023-11-19 20:09:17 +0700

    update home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,31 @@
 
 @section("content")
 
-HOME
+<div class="section-banner">
+    <div class="list">
+        <div class="item">
+            <img src="{{ asset('storage/image/home/banner1.jpg') }}" alt="" class="w-100 rounded-4 img-fluid">
+        </div>
+        <div class="item">
+            <img src="{{ asset('storage/image/home/banner2.jpg') }}" alt="" class="w-100 rounded-4 img-fluid">
+        </div>
+        <div class="item">
+            <img src="{{ asset('storage/image/home/banner3.jpg') }}" alt="" class="w-100 rounded-4 img-fluid">
+        </div>
+        <div class="item">
+            <img src="{{ asset('storage/image/home/banner4.jpg') }}" alt="" class="w-100 rounded-4 img-fluid">
+        </div>
+    </div>
+
+    {{-- Button Prev & Next --}}
+    <div class="controls">
+        <div class="prev">
+
+        </div>
+        <div class="next">
+
+        </div>
+    </div>
+</div>
 
 @endsection
