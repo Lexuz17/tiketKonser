9444514203babf0485534955dbda5682cc4a57af
Daprsy <149225279+Daprsy@users.noreply.github.com>
2023-12-02 13:00:56 +0700

    Update transaction_history.blade.php

--- resources/views/transaction_history.blade.php
+++ resources/views/transaction_history.blade.php
@@ -3,9 +3,50 @@
 @section('title', 'history')
 
 @section("content")
-
-History Transaction
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<!-- History Transaction
 ref:
-https://www.loket.com/member/tiket-saya
+https://www.loket.com/member/tiket-saya -->
+
+<h1>History Transaction</h1>
+
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-0 d-flex align-items-center justify-content-center">
+    
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title fw-bold ">PESTAPORA 2022</h3>
+        <div data-v-fd7fe0b8="" class="additional"><div data-v-fd7fe0b8="" class="additional-date"><i data-v-fd7fe0b8="" class="ai ai-calendar"></i>
+        <i class="fa-regular fa-calendar-days"></i>   23 Sep - 25 Sep 2022
+        </div> <div data-v-fd7fe0b8="" class="additional-separator"></div> <div data-v-fd7fe0b8="" class="additional-ticket-qty"><i data-v-fd7fe0b8="" class="ai ai-ticket"></i>
+        <i class="fa-solid fa-ticket"></i> 2 Tiket
+        </div></div>
+        <div data-v-fd7fe0b8="" class="invoice-date">Pembelian pada 08 Jul 2022, 19:53</div>
+
+        <button class="btn btn-primary" type="submit">E-Voucher</button>
+        <button class="btn btn-primary" type="submit">Invoice</button>
+
+
+      </div>
+    </div><div class="col-md-4">
+      <img src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20220705053307.jpg" class="img-fluid rounded-start" alt="PESTAPORA 2022">
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
+
 
 @endsection
